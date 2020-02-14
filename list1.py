8e2708b54365f43fc8146896f5de9f855a3921e9
Binup karki <karki011@gmail.com>
2020-02-13 20:57:47 -0600

    done with list1

--- list1.py
+++ list1.py
@@ -26,8 +26,11 @@ Kenzie List1 Assignment
 
 
 def match_ends(words):
-    # your code here
-    return
+    matches = 0
+    for word in words:
+        if len(word) >= 2 and word[0] == word[-1]:
+            matches += 1
+    return matches
 
 
 # B. front_x
@@ -38,9 +41,17 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    # your code here
-    return
-
+    xwords = []
+    notxwords = []
+    for word in words:
+        if word[0] == 'x':
+            xwords.append(word)
+        else:
+            notxwords.append(word)
+    xwords.sort()
+    notxwords.sort()
+    xwords.extend(notxwords)
+    return xwords
 
 # C. sort_last
 # Given a list of non-empty tuples, return a list sorted in increasing
@@ -48,9 +59,10 @@ def front_x(words):
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
+def last(n): return n[-1]
+
 def sort_last(tuples):
-    # your code here
-    return
+    return sorted(tuples, key=last)
 
 
 # Simple provided test() function used in main() to print
